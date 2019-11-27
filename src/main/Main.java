6283dca700b0369721389db929184f44d687fceb
Nikola <nikola.carovic@yahoo.com>
2019-11-27 21:01:54 +0100

    Glavini meni dodat

---
+++ src/main/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import javax.swing.JFrame;
+import ui.form.FLogin;
+
+/**
+ *
+ * @author Nikola
+ */
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new FLogin();
+        frame.setVisible(true);
+    }
+}
