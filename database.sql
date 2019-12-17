fb4b8b1a9be9c41c75959ffcadc27c5b51e4caf2
Nikola <nikola.carovic@yahoo.com>
2019-12-17 19:29:38 +0100

    Ubacen database file

--- database.sql
+++ database.sql
@@ -18,13 +18,35 @@ USE `seminarskibiblioteka`;
 
 /*Data for the table `bibliotekar` */
 
+insert  into `bibliotekar`(`korisnicko_ime`,`sifra`,`ime_prezime`,`email`,`jmbg`,`sifra_zaposlenog`,`broj_telefona`) values 
+('Bibliotekar1','sifra','Slika Slike','email@email.com','2311990123321',1,'069000000');
+
 /*Data for the table `knjiga` */
+
+insert  into `knjiga`(`isbn`,`naslov`,`autor`,`izdavac`,`jezik`,`opis`,`broj_strana`,`ukupan_broj_knjiga`,`raspoloziv_broj_knjiga`,`prosecna_ocena`,`vlasnik`) values 
+(1,'Knjiga1','Autor1','Izdavac1','Srpski','Ovde ide neki opsi',100,100,100,5,'Bibliotekar1'),
+(2,'Knjiga2','Autro2','Izdavac2','Engleski','I ovde ide opis',200,50,50,4,'Bibliotekar1');
 
 /*Data for the table `korisnik` */
 
+insert  into `korisnik`(`korisnicko_ime`,`sifra`,`ime_prezime`,`email`,`jmbg`,`status`,`broj_aktivnih_iznajmljivanja`) values 
+('Korisnik1','sifra','Korisnik K','emailneki@gmail.com','1111111111111','aktivan',0),
+('Korisnik2','sifra','Korisnik 2','email2@gmail.com','2222222222222','aktivan',0);
+
 /*Data for the table `ocena` */
 
+insert  into `ocena`(`isbn_knjige`,`korisnicko_ime_korisnika`,`konacna_ocena`,`opis`) values 
+(1,'Korisnik1',5,'Dobra knjiga'),
+(1,'Korisnik2',4,'Ovde isto ide opis neki'),
+(2,'Korisnik1',3,'Ovde ide opis');
+
 /*Data for the table `rezervacija` */
+
+insert  into `rezervacija`(`isbn_knjige`,`korisnicko_ime_korisnika`,`datum_od`,`datum_do`,`status`) values 
+(1,'Korisnik1','2019-12-13','2019-12-20','vracena'),
+(2,'Korisnik1','2010-10-10','2019-12-20','vracena'),
+(44,'Korisnik2','2001-01-01','2019-12-20','vracena'),
+(44,'Korisnik2','2010-10-10','2019-12-20','vracena');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
